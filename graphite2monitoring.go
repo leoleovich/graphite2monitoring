8273f71c1d524b18567d7412efed73d45e56fc36
Oleg Obleukhov <oleg.obleukhov@innogames.com>
2016-04-04 11:32:27 +0200

    Use relative time instead of absolute in graphite query

--- graphite2monitoring.go
+++ graphite2monitoring.go
@@ -8,7 +8,7 @@ import (
 	"encoding/hex"
 	"flag"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func new_token(username string, timestamp int, secret string) string {
@@ -17,25 +17,24 @@ func new_token(username string, timestamp int, secret string) string {
 	h.Write([]byte(unauthed_token))
 	return fmt.Sprint(hex.EncodeToString(h.Sum(nil)), ":", timestamp, ":", username)
 }
-func converTime2GraphiteFormat(time2convert int64) string {
-	timeRange := time.Unix(time.Now().Unix() - time2convert, 0)
-	return strings.Replace(timeRange.UTC().Format("15:04 20060102")," ","_", -1) //10:0020150923
+func converTime2GraphiteFormat(time2convert int) string {
+	return "-" + strconv.Itoa(time2convert) + "s" // -3600s
 }
 
 func main() {
 	var username, authToken, metric, url string
 	var thresholdWarningI, thresholdCriticalI, thresholdWarningD, thresholdCriticalD int
-	var range1FromAgo, range1UntilAgo, range2FromAgo, range2UntilAgo int64
+	var range1FromAgo, range1UntilAgo, range2FromAgo, range2UntilAgo int
 	var debug bool
 	flag.StringVar(&username, "u", "graphite", "User, which has rights to access Graphite")
 	flag.StringVar(&authToken, "a", "", "AuthToken to access the graphite-API. For example 'qqq'")
 	flag.StringVar(&metric, "m", "", "Name of metric or metric filter e.g. qqqq.test.leoleovich.currentProblems")
 	flag.StringVar(&url, "U", "", "Base address of your graphite server e.g. https://graphite.protury.info/")
 
-	flag.Int64Var(&range1FromAgo, "range1From", 90000, "Amount of seconds ago for the 1st range (from)")
-	flag.Int64Var(&range1UntilAgo, "range1Until", 86400, "Amount of seconds ago for the 1st range (until)")
-	flag.Int64Var(&range2FromAgo, "range2From", 3600, "Amount of seconds ago for the 2st range (from)")
-	flag.Int64Var(&range2UntilAgo, "range2Until", 0, "Amount of seconds ago for the 2st range (until)")
+	flag.IntVar(&range1FromAgo, "range1From", 90000, "Amount of seconds ago for the 1st range (from)")
+	flag.IntVar(&range1UntilAgo, "range1Until", 86400, "Amount of seconds ago for the 1st range (until)")
+	flag.IntVar(&range2FromAgo, "range2From", 3600, "Amount of seconds ago for the 2st range (from)")
+	flag.IntVar(&range2UntilAgo, "range2Until", 0, "Amount of seconds ago for the 2st range (until)")
 
 	flag.IntVar(&thresholdWarningI, "wi", 20, "Metrics above this threshold will be marked as warning")
 	flag.IntVar(&thresholdCriticalI, "ci", 40, "Metrics above this threshold will be marked as critical")
